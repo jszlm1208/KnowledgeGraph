a25b963bbcad4c0683107b04d4370ddafe3c90b0
Limei Zhang <limzhan@microsoft.com>
2022-10-28 21:53:22 -0700

    update infer run script

--- WikiKG90M/python/run_infer_all.sh
+++ WikiKG90M/python/run_infer_all.sh
@@ -2,9 +2,12 @@ export DGLBACKEND=pytorch
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 
+export data_path="/mnt/data/ogbl_wikikg2_recent/ogbl_wikikg2/smore_folder_full"
+export model_path="/mnt/data/ogbl_wikikg2_recent/ogbl_wikikg2/smore_folder_full/saved_models/ote/OTE_wikikg90m_concat_d_240_g_12.00"
+
 infer(){
 
-python infer.py --data_path $data_path --infer_valid --model_path $model_path &
+python models/infer.py --data_path $data_path --infer_valid --model_path $model_path &
 sleep 600
 ps -ef|grep infer.py | awk '{print $2}'| xargs kill -9
 
